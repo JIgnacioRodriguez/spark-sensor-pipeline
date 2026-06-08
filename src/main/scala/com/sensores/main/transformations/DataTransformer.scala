f1cacbd684af65d97fff3626a2768473812246b5
JIgnacioRodriguez <ignacio.rodriguez.palomares@gmail.com>
2026-06-08 10:29:02 +0200

    Error trycatch supported

--- src/main/scala/com/sensores/main/transformations/DataTransformer.scala
+++ src/main/scala/com/sensores/main/transformations/DataTransformer.scala
@@ -2,22 +2,23 @@ package com.sensores.main.transformations
 
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.col
+import scala.util.{Try, Success, Failure}
 
 trait Transformer {
-  def transform(df: DataFrame): DataFrame
+  def transform(df: DataFrame): Either[Throwable, DataFrame]
 }
 
 // Implementación con parámetros inyectados en el constructor
 class HighTemperatureFilter(threshold: Double) extends Transformer {
-  override def transform(df: DataFrame): DataFrame = {
+  override def transform(df: DataFrame): Either[Throwable, DataFrame] = {
     // Usar 'col' es una buena práctica frente a df("nombre")
-    df.filter(col("temperatura") > threshold)
+    Try(df.filter(col("temperatura") > threshold)).toEither
   }
 }
 
 // Ejemplo de transformación de columnas
 class FormatColumnTransformer(targetColumn: String) extends Transformer {
-  override def transform(df: DataFrame): DataFrame = {
-    df.withColumnRenamed(targetColumn, targetColumn.toLowerCase)
+  override def transform(df: DataFrame): Either[Throwable, DataFrame] = {
+    Try(df.withColumnRenamed(targetColumn, targetColumn.toLowerCase)).toEither
   }
 }
